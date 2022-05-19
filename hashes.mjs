803a656124b201f84742dab90c437b72d9bb1082
paoloumali <2522851+paoloumali@users.noreply.github.com>
2022-05-19 05:25:56 +0000

    hash elem increment

--- hashes.mjs
+++ hashes.mjs
@@ -1,6 +1,7 @@
 const Hashes = (redis) => {
   redis.hmset('user:1', 'name', 'Paolo', 'age', 35);
   redis.hgetall('user:1', (err, result) => console.log(result));
+  redis.hincrby('user:1', 'age', 1);
 }
 
 export default Hashes;
